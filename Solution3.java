ff908f98ae249ea7bedf51d87bf5951551aa7acc
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2025-07-31 14:12:57 +0530

    Add files via upload

---
+++ Solution3.java
@@ -0,0 +1,13 @@
+import java.util.Set;
+
+public class Solution3 {
+ public static void main(String[] args) {
+ int x=5, y=10;
+if(x>0 && y<15){
+  System.out.println(x+y);
+}
+else{
+  System.out.println(x*y);
+}
+ } 
+}
