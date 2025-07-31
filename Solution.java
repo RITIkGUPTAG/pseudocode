57edbd7231b2ff65341adc112bc27772162aa95f
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2025-07-31 14:44:29 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+  public static void main(String[] args) {
+    int value=2;
+    int sum=0;
+    while(value <=10){
+      sum+=value;
+      value +=2;
+    }
+    System.out.println(sum);
+  }
+}
